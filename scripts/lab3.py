153cdf78f551e81a5ad548ffb9a7eed57ba3bdbf
lgstuehrmann <lgstuehrmann18@gmail.com>
2017-04-13 01:56:39 -0400

    navwithastar ready for testing

--- scripts/lab3.py
+++ scripts/lab3.py
@@ -10,6 +10,7 @@ from geometry_msgs.msg import PoseStamped, Pose, Point, PoseWithCovarianceStampe
 from kobuki_msgs.msg import BumperEvent
 from std_msgs.msg import String
 from tf.transformations import euler_from_quaternion
+from numpy import arctan
 # Add additional imports for each of the message types used
 
 wheel_base = 0.2286 #m
@@ -97,6 +98,8 @@ def getStart(_startPos):
     print startPos.pose.pose
     point = getWorldPointFromIndex(startIndex)
     print "Calculated world position: %f, %f Index: %i" % (point.x, point.y, startIndex)
+
+    pass
 
 def readGoal(goal):
     global goalRead
@@ -317,8 +320,7 @@ def aStar():
     print len(openSet)
     #print openSet[0].index
 
-    while openSet:  
-
+    while openSet:
         try:
             i = lowestInQ(openSet) 
             current = G[i]
@@ -484,31 +486,34 @@ def pubGoal(grid):
     goal_pub.publish(cells)
 
 #drive to a goal subscribed as /move_base_simple/goal
-def navToPose(goal):
-    #take goal params and find delta to currpos params
-    print "spin!"
-    #turn delta theta using spinWheels(vr, -vr, time to rotate)
+#cry
 
-    print "move!"
-    #go delta x, y using spingWheels(vr, vl, time to dest)
-    
-    print "spin!"
-    #spin delta theta using spingWheels(vr, -vr, time to dest)
-    
-    print "done"
-    #cry
-    pass
 
 #Odom "Callback" function.
 def readOdom(msg):
     global pose
+    pose = msg
+    global startRead
+    global startPosX
+    global startPosY
+    global startPos
+    global startIndex
+    startPos = pose
+    startPosX = startPos.pose.pose.position.x
+    startPosY = startPos.pose.pose.position.y
 
-    pose = msg.pose
+    startIndex = getIndexFromWorldPoint(startPosX, startPosY)
 
+    point = getWorldPointFromIndex(startIndex)
+    
+    startIndex = getIndexFromWorldPoint(startPosX, startPosY)
+    #print "Printing start pose"
+    print startPos.pose.pose
+    point = getWorldPointFromIndex(startIndex)
+    print "Calculated world position: %f, %f Index: %i" % (point.x, point.y, startIndex)
 
 #This function sequentially calls methods to perform a trajectory.
 def executeTrajectory():
-
     driveStraight(0.3, 0.06)
     rotate(1.6)
     driveStraight(0.3, 0.045)
@@ -520,16 +525,13 @@ def spinWheels(u1, u2, time):
     w = (u1 - u2) / wheel_base
     u = (wheel_diam / 2) * (u1 + u2)
     start = rospy.Time().now().secs
-
     while ((rospy.Time().now().secs - start) < time):
         publishTwist(u, w)
-
     publishTwist(0, 0)
 
 #This function accepts a speed and a distance for the robot to move in a straight line
 
 def driveStraight(speed, distance):
-
     global pose
 
     xnaught = pose.pose.position.x #set an origin at the robot's current position
@@ -547,18 +549,11 @@ def driveStraight(speed, distance):
             print "fuken dun m8"
 
         else:
-
-            #currpos.linear.x = speed
-            #pub.publish(currpos)
             publishTwist(speed, 0)
             print "moving"
-            #rospy.sleep(rospy.Duration(0, 0.02))
-        #rospy.sleep(rospy.Duration(0, 500000))
-
 
 #Accepts an angle and makes the robot rotate around it.
 def rotate(angle):
-
     global odom_list
     global pose
 
@@ -566,22 +561,17 @@ def rotate(angle):
     rotation = numpy.array([[math.cos(angle), -math.sin(angle), 0], #Create th goal rotation
                             [math.sin(angle), math.cos(angle), 0],
                             [0,          0,          1]])
-
- 
-
-#Get all the transforms for frames
+    #Get all the transforms for frames
     odom_list.waitForTransform('odom', 'base_footprint', rospy.Time(0), rospy.Duration(4.0))
     (trans, rot) = odom_list.lookupTransform('odom', 'base_footprint', rospy.Time(0))
     T_o_t = transformer.fromTranslationRotation(trans, rot)
     R_o_t = T_o_t[0:3,0:3]
-
- #Setup goal matrix
+    #Setup goal matrix
     goal_rot = numpy.dot(rotation, R_o_t)
     goal_o = numpy.array([[goal_rot[0,0], goal_rot[0,1], goal_rot[0,2], T_o_t[0,3]],
                  [goal_rot[1,0], goal_rot[1,1], goal_rot[1,2], T_o_t[1,3]],
                  [goal_rot[2,0], goal_rot[2,1], goal_rot[2,2], T_o_t[2,3]],
                  [0,             0,             0,             1]])
-
     #This code continuously creates and matches coordinate transforms.
     done = False
     while (not done and not rospy.is_shutdown()):
@@ -597,20 +587,13 @@ def rotate(angle):
             else:
                 spinWheels(-.1,.1,.1)
 
-#This function works the same as rotate how ever it does not publish linear velocities.
-def driveArc(radius, speed, angle):
-    pass  # Delete this 'pass' once implemented
-
 #This function takes angular and linear speeds and publishes them to a twist-type message
 def publishTwist(linearvalue, angularvalue):
-
     global pub
-
     twist = Twist()
     twist.linear.x = linearvalue
     twist.angular.z = angularvalue
-
-    pub.publish(twist)
+    pub_vel.publish(twist)
 
 
 #Bumper Event Callback function
@@ -631,6 +614,57 @@ def timerCallback(event):
     
     pass # Delete this 'pass' once implemented
 
+###############################################LAB 4 FUNCTIONS################################
+#Expand obstacles
+#Assumes robot has a 5 gridcell radius
+#Map should be an array of gridcells
+"""
+def expandObstacles(map):
+    get the first cell
+    while cells have not been checked
+        if cellvalue >=0
+            add to checkedmap
+        else if cellvalue < 0
+            check cells in 5-cell radius
+            if any cell in radius .value >0
+                skip it
+            else cellsinradius.value = -1
+                add all to checkedmap
+    
+    return checkedmap
+"""
+
+def navWithAStar():
+    global pose 
+
+    path = aStar()
+    publishPath(noFilter(path))
+    publishWaypoints(getWaypoints(path))
+    newpathPoints = getWaypoints(path)
+    posePath = list()
+    for point in newpathPoints:
+        newPose = (msg.pose.pose.position.x,
+                msg.pose.pose.position.y)
+        posePath.add(newPose)
+    donePoses = list()
+
+    for pose in posePath:
+        #for distance
+        desx = newPose.pose.position.x
+        desy = newPose.pose.position.y
+        thisx = pose.pose.position.x
+        thisy = pose.pose.position.y
+        deltax = desx-thisx
+        deltay = desy-thisy
+        distancetoTraverse=pow((pow(deltax,2)+pow(deltay,2)),.5)
+        #for angle to rotate
+        phi=arctan(desy/desx)
+        thisphi = arctan(thisy/thisx)
+        angletoRotate = (180-phi)-thisphi
+        rotate(angletoRotate)
+        driveStraight(distancetoTraverse)
+        donePoses.add(newPose)
+        posePath.remove(newPose)
 
 # This is the program's main function
 if __name__ == '__main__':
@@ -656,11 +690,11 @@ if __name__ == '__main__':
 
     # Change this node name to include your username
     rospy.init_node('lab3')
+   
+    pub_vel = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, None, queue_size=10) # Publisher for commanding robot motion
+    bumper_sub = rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, readBumper, queue_size=1) # Callback function to handle bumper events
+    odom_sub = rospy.Subscriber('/odom', Odometry, readOdom)
 
-    # Replace the elipses '...' in the following lines to set up the publishers and subscribers the lab requires
-    
-    
-    bumper_sub = rospy.Subscriber('mobile_base/events/bumper', BumperEvent, readBumper, queue_size=1) # Callback function to handle bumper events
     sub = rospy.Subscriber('/map', OccupancyGrid, mapCallBack)
     pub = rospy.Publisher('/mapcheck', GridCells) # Publisher for commanding robot motion
     pub_path = rospy.Publisher('/path', GridCells)
@@ -670,30 +704,18 @@ if __name__ == '__main__':
     goal_sub = rospy.Subscriber('/goalpose', PoseStamped, readGoal)
     pub_traverse = rospy.Publisher('/traversal', GridCells, queue_size=1)
     pub_frontier = rospy.Publisher('/frontier', GridCells, queue_size=1)
-    start_sub = rospy.Subscriber("/startpose", PoseWithCovarianceStamped, getStart, queue_size=1) #change topic for best results
     goal_pub = rospy.Publisher("/goalpose", PoseStamped, queue_size=1)
     # wait a second for publisher, subscribers, and TF
     rospy.sleep(1)
     # Use this object to get the robot's Odometry 
-    
     odom_list = tf.TransformListener()
-    #odom_tf = tf.TransformBroadcaster()
-    #odom_tf.sendTransform((0, 0, 0),(0, 0, 0, 1),rospy.Time.now(),"base_footprint","odom")
-
-
-    # Use this command to make the program wait for some seconds
     
     print "Starting Lab 3"
 
     while (1 and not rospy.is_shutdown()):
         publishCells(mapData) #publishing map data every 2 seconds
-        if startRead and goalRead:
-            path = aStar()
-            print "Going to publish path"
-            publishPath(noFilter(path))
-            print "Publishing waypoints"
-            publishWaypoints(getWaypoints(path))#publish waypoints
-            print "Done!"
+        if goalRead:
+            navWithAStar()
             goalRead = False
         rospy.sleep(2)
 
